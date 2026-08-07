1b4e4750e85dafb365f6f5ef944782b262221659
Muhammed Fahad <fahadsheikimam@gmail.com>
2026-08-07 07:22:40 +0530

    file_added

--- LinkedList/Doubly_LL/Delete_HeadDL.py
+++ LinkedList/Doubly_LL/Delete_HeadDL.py
@@ -3,3 +3,31 @@ class DDLinkedList:
         self.prev = prev
         self.val = val
         self.next = next
+
+def Deletehead(head):
+    nextnode = head.next
+    nextnode.prev = None
+    printDDL(nextnode)
+
+def printDDL(head):
+    current = head
+    while(current):
+        print(current.val, end=" <-> ")
+        current = current.next
+    return
+
+if __name__ == "__main__":
+    head = DDLinkedList(1)
+    second = DDLinkedList(2)
+    third = DDLinkedList(3)
+    fourth = DDLinkedList(4)
+
+    head.next = second
+    second.prev = head
+
+    second.next = third
+    third.prev = second
+
+    third.next = fourth
+    fourth.prev = third
+    Deletehead(head)
